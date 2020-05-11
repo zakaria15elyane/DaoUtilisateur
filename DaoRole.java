2a9e212c8f9bbb35ae8396eb83b9819ad66d12cd
zakariaElayane <zakaria15elyane@gmail.com>
2020-05-11 22:21:45 +0000

    role

--- DaoRole.java
+++ DaoRole.java
@@ -8,59 +8,52 @@ import java.util.ArrayList;
 import com.mysql.jdbc.PreparedStatement;
 
 import ma.map.database.DbConnexion;
+import ma.map.model.ModelRole;
 import ma.map.model.ModelUtilisateur;
 
 public class DaoRole {
 
 	private static DbConnexion dbConnexion = DbConnexion.getInstance();
 	
-	 public ArrayList<ModelUtilisateur> getAllRole() throws SQLException {
-	        ArrayList<ModelUtilisateur> role = new ArrayList<ModelUtilisateur>();
+	 public ArrayList<ModelRole> getAllRole() throws SQLException {
+	        ArrayList<ModelRole> roles = new ArrayList<ModelRole>();
 	        Connection conn = DbConnexion.getConnection();
      	PreparedStatement ps =(PreparedStatement) conn.prepareStatement("SELECT * FROM role");
      	
      	ResultSet rs = ps.executeQuery();
 	        while (rs.next()) {
-	            ModelUtilisateur utilisateur = new ModelUtilisateur();
-	            utilisateur.setId(rs.getInt("id"));
-	            utilisateur.setNom(rs.getString("nom"));
-	            utilisateur.setPrenom(rs.getString("prenom"));
-	            utilisateur.setEmail(rs.getString("email"));    
-	            utilisateur.setRole(rs.getString("role"));
-	            utilisateur.setEtat(rs.getInt("etat"));
-	         
-	            role.add(utilisateur);
+	            ModelRole role = new ModelRole();
+	            role.setId_role(rs.getInt("id_role"));
+	            role.setRole(rs.getString("role"));
+	            roles.add(role);
 	        }
-	        return role;
+	        return roles;
 	    }
-	 public ModelUtilisateur getUtilisateurRoleById(int id) throws SQLException {
+	 public ModelRole getUtilisateurRoleById(int id) throws SQLException {
     	 Connection conn = DbConnexion.getConnection();
       
-         ModelUtilisateur utilisateur = null;
+         ModelRole role = null;
          try {
         	 PreparedStatement ps =(PreparedStatement) conn.prepareStatement("SELECT * FROM `role` WHERE id_role = ?");
 
              ps.setInt(1, id);
              ResultSet res = ps.executeQuery();
              if (res.next()) {
-                 utilisateur = new ModelUtilisateur();
-                 utilisateur.setId(res.getInt("id"));
-                 utilisateur.setNom(res.getString("nom"));
-                 utilisateur.setPrenom(res.getString("prenom"));
-                 utilisateur.setEmail(res.getString("email"));
-                 utilisateur.setRole(res.getString("role"));
-                 utilisateur.setEtat(res.getInt("etat"));
+                 role = new ModelRole();
+                 role.setId_role(res.getInt("id_role"));
+                 role.setRole(res.getString("role"));
+                
              }
          } catch (Exception e) {
              System.out.println(e);
          }
-         return utilisateur;
+         return role;
      }
 	 
-	 public ModelUtilisateur getRoleById(int id) throws SQLException {
+	 public ModelRole getRoleById(int id) throws SQLException {
     	 Connection conn = DbConnexion.getConnection();
       
-         ModelUtilisateur utilisateur = null;
+         ModelRole role = null;
          try {
         	 PreparedStatement ps =(PreparedStatement) conn.prepareStatement("SELECT * FROM `role` WHERE id = ?");
              
@@ -68,17 +61,14 @@ public class DaoRole {
              ps.setInt(1, id);
              ResultSet res = ps.executeQuery();
              if (res.next()) {
-                 utilisateur = new ModelUtilisateur();
-                 utilisateur.setId(res.getInt("id"));
-                 utilisateur.setNom(res.getString("nom"));
-                 utilisateur.setPrenom(res.getString("prenom"));
-                 utilisateur.setEmail(res.getString("email"));
-                 utilisateur.setRole(res.getString("role"));
-                 utilisateur.setEtat(res.getInt("etat"));
+                 role = new ModelRole();
+                 role.setId_role(res.getInt("id_role"));
+                 role.setRole(res.getString("role"));
+                 
              }
          } catch (Exception e) {
              System.out.println(e);
          }
-         return utilisateur;
+         return role;
      }
 }
